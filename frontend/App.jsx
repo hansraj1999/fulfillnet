64002fef0f8485b3251c3e65b2c3d6e033425bbd
abhishekgupta01 <abhishekgupta@gofynd.com>
2025-02-06 19:13:42 +0530

    proflile fill handling

--- frontend/App.jsx
+++ frontend/App.jsx
@@ -6,6 +6,7 @@ import Button from "./components/Button";
 // import { getCompany } from "./Utilities/company.util";
 import BreadCrumb from "./components/BreadCrumb/BreadCrumb";
 import MainService from "./services/main-service";
+import InfoIcon from "./public/assets/Info.svg";
 // import BidOrder from "./components/BidOrder";
 
 const HeaderComponent = styled.div`
@@ -62,8 +63,26 @@ const ButtonComponent = styled(Button)`
   border-radius: 12px;
 `;
 
+const NoteWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 8px;
+
+  background: #fff3cd;
+  padding: 8px;
+  border-radius: 8px;
+  border: 1px solid #f8da85;
+`;
+const NoteIcon = styled.div`
+  font-size: 12px;
+  font-weight: bold;
+  color: #664d03;
+`;
+const NoteText = styled.div``;
+
 const CustomCards = (props) => {
-  const { item, onClick, disabled } = props;
+  const { item, onClick, disabled, userVeirfied } = props;
   const { title, desc, buttonText } = item || {};
   return (
     <>
@@ -73,7 +92,11 @@ const CustomCards = (props) => {
           <Desc>{desc}</Desc>
         </CardBody>
         <CardFooter>
-          <ButtonComponent disabled={disabled} size="small" onClick={onClick}>
+          <ButtonComponent
+            disabled={item?.route === "profile" ? false : disabled}
+            size="small"
+            onClick={onClick}
+          >
             {buttonText}
           </ButtonComponent>
         </CardFooter>
@@ -85,7 +108,7 @@ const CustomCards = (props) => {
 function App() {
   const { company_id } = useParams();
   const navigate = useNavigate();
-  const [isRegistered, setRegistered] = useState(false);
+  const [userVeirfied, setUserVerified] = useState(true);
   const [actionCards, setActionCards] = useState([
     {
       title: "My Bids",
@@ -120,6 +143,8 @@ function App() {
   ]);
 
   const handleCompanyRegisteration = async () => {
+    setUserVerified(false);
+
     const result = await MainService.registerCompany({
       company_id,
     });
@@ -132,14 +157,27 @@ function App() {
         closeOnClick: false,
       });
     }
+  };
 
-    setRegistered(true);
+  const getCompanyProfile = async () => {
+    const result = await MainService.getProfileDetailsByCompanyID({
+      company_id,
+    });
+    const { data: profileDetails } = result?.data;
+
+    if (!profileDetails) {
+      handleCompanyRegisteration();
+    } else {
+      const isVerified = profileDetails?.account_number;
+
+      if (!isVerified) {
+        setUserVerified(false);
+      }
+    }
   };
 
   useEffect(() => {
-    if (!isRegistered) {
-      handleCompanyRegisteration();
-    }
+    getCompanyProfile();
   });
 
   const handleClick = (route) => {
@@ -148,29 +186,41 @@ function App() {
 
   return (
     <>
-      <HeaderComponent>
-        <BreadCrumb
-          breadCrumbList={[
-            {
-              key: "home",
-              label: "Home",
-              link: "current",
-            },
-          ]}
-        />
-      </HeaderComponent>
-      <Wrapper>
-        {actionCards?.map((item, idx) => {
-          return (
-            <CustomCards
-              key={idx}
-              item={item}
-              onClick={() => handleClick(item.route)}
-              disabled={!isRegistered}
-            />
-          );
-        })}
-      </Wrapper>
+      {!userVeirfied && (
+        <NoteWrapper>
+          <NoteIcon>
+            <img src={InfoIcon} />
+          </NoteIcon>
+          <NoteIcon>Please fill the profile details</NoteIcon>
+        </NoteWrapper>
+      )}
+
+      <>
+        <HeaderComponent>
+          <BreadCrumb
+            breadCrumbList={[
+              {
+                key: "home",
+                label: "Home",
+                link: "current",
+              },
+            ]}
+          />
+        </HeaderComponent>
+        <Wrapper>
+          {actionCards?.map((item, idx) => {
+            return (
+              <CustomCards
+                key={idx}
+                item={item}
+                onClick={() => handleClick(item.route)}
+                userVeirfied={userVeirfied}
+                disabled={!userVeirfied}
+              />
+            );
+          })}
+        </Wrapper>
+      </>
 
       <ToastContainer />
       {/* <BidOrder /> */}
